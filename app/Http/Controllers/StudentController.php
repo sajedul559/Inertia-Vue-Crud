5e87728fc44355a922cf9080fef6c4a80daadf31
sajedul559 <sajedulkhairul@gmail.com>
2023-09-14 00:13:37 +0600

    nothing

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -89,8 +89,8 @@ class StudentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    // public function destroy(string $id)
-    // {
-    //     //
-    // }
+    public function destroy(string $id)
+    {
+        //
+    }
 }
